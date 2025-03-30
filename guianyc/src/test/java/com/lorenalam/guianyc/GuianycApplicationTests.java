accd900b4dcfc7fe994fb2c60956f5fb788ce41a
lorenalam <202108747@alu.comillas.edu>
2025-03-30 19:58:00 +0200

    Subida inicial archivos

---
+++ guianyc/src/test/java/com/lorenalam/guianyc/GuianycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenalam.guianyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuianycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
